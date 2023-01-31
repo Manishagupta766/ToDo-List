80ec4d0f6bd49bb73451da06fe7f154fa897d9d8
Manisha Gupta <guptamanisha8835@gmail.com>
2023-01-31 09:45:53 +0530

    First commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css"
+const Header = () => {
+    return (
+        <header>
+            <h1>To Do</h1>
+            <p>What do you want to do today?</p>
+        </header>
+        
+    );
+};
+
+export default Header;
